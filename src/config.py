468af3cc86ab737f89cfb909eb7e206012d4b7be
JacoR <jacopo.024@gmail.com>
2025-04-03 17:30:44 +0200

    Changes

--- src/config.py
+++ src/config.py
@@ -6,14 +6,11 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # Paths for raw data (Excel files)
 RAW_DATA_PATH = os.path.join(BASE_DIR, "../data/raw/")
 
-# SQLite Database Path
-DATABASE_PATH = os.path.join(BASE_DIR, "../database/sentiment.db")
-
 # Preprocessed Data Table Name
 PROCESSED_TABLE = "processed_tweets"
 
 # Raw Data Table Name
-RAW_TABLE = "raw_tweets"
+RAW_TABLE = "raw_house"
 
 # Predictions Table Name
 PREDICTIONS_TABLE = "predictions"
